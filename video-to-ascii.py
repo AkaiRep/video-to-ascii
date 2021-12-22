03b1cb0bbe1da994ff3484e5121cacf741d33aa5
Redreev Maxim <MAaxim91@gmail.com>
2021-12-23 02:39:58 +0500

    Update: Continue refactoring

--- video-to-ascii.py
+++ video-to-ascii.py
@@ -6,11 +6,15 @@ import cv2
 from PIL import Image
 
 
-columns,lines = 140,70
+video_columns, video_lines = 140, 70
 rev = True
 
+def set_console_size(columns, lines):
+    os.system(f'mode con cols={columns} lines={lines} ')
+
+
 os.system('cls')
-os.system('mode con cols=40 lines=20 ')
+set_console_size(40, 20)
 selected_video_number = 0
 
 videos = glob.glob('*.mp4')
@@ -23,14 +27,14 @@ selected_video_number = input('\nВведите номер видео: ')
 try:
     selected_video = videos[int(selected_video_number) - 1]
 except:
-    os.system('mode con cols=100 lines=30 ')
+    set_console_size(100, 30)
     print(f'{selected_video_number} - неверный номер X_X')
     exit()
 
 vidcap = cv2.VideoCapture(selected_video)
 success, image = vidcap.read()
 
-os.system(f'mode con cols={columns} lines={lines}')
+set_console_size(video_columns, video_lines)
 symbols = list(r'$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1[]?-_+~<>i!lI;:,   ')
 
 if rev:
@@ -39,14 +43,18 @@ if rev:
 try:
     while True:
         success, image = vidcap.read()
+        
+        if not success:
+            break
+
         im = Image.fromarray(image)
-        im = im.resize((columns, lines))
+        im = im.resize((video_columns, video_lines))
         im = im.convert('L')
         pixels = im.load()
         result = []
 
-        for y in range(1, lines):
-            for x in range(1, columns):
+        for y in range(1, video_lines):
+            for x in range(1, video_columns):
                 result.append(symbols[int(pixels[x, y] / 36) - 1])
             result.append('\n')
 
@@ -54,4 +62,4 @@ try:
 
         sleep(0.016)
 except KeyboardInterrupt:
-    os.system("mode con cols=100 lines=30 ")
+    set_console_size(100, 30)
